181a592b8d7a180914d18c4b58ff96d27c11da30
keerthanatutika06 <tutikakeerthanasai@gmail.com>
2024-09-12 19:19:28 +0530

    updated lunacal solution

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-// src/App.js
 import React from 'react';
-// import { BrowserRouter, Route, Routes } from 'react-router-dom'; 
 import Home from './components/Home';
-import './App.css';
 
 function App() {
-  return (
-      <div className="App">
-          <Home/>
-      </div>
-  );
+    return (
+        <div className="App">
+           <Home />
+        </div>
+    );
 }
 
 export default App;
